dcebc7ca074dcd89b5ec16e8ce8a48247777ae30
smarquis20 <shaunmarquis@gmail.com>
2025-02-07 01:28:07 -0600

    Week05 Discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,7 +1,9 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
 const hbutton = document.querySelector('#hambutton');
+const navElement = document.querySelector('.nav-menu');
 const discoverPlace = document.querySelector('#discover-place');
+const visitMessage = document.querySelector('#visit-message');
 const url = 'data/discover.json';
 
 hbutton.addEventListener('click', () => {
@@ -14,7 +16,6 @@ hbutton.addEventListener('click', () => {
     const data = await response.json();
     places = data.places;
 
-    console.log(places);
     displayPlaces(places);
 }
 
@@ -24,11 +25,12 @@ const displayPlaces = (places) => {
 
     places.forEach((place) => {
         let card = document.createElement('div');
-        let name = document.createElement('h3');
+        let name = document.createElement('h2');
+        let figure = document.createElement('figure');
         let photo = document.createElement('img');
-        let address = document.createElement('p');
-        let cost = document.createElement('p');
+        let address = document.createElement('address');
         let description = document.createElement('p');
+        let learn = document.createElement('button');
 
         card.classList.add('discoverPlaceCard');
 
@@ -38,19 +40,47 @@ const displayPlaces = (places) => {
         photo.setAttribute('loading', 'lazy');
         photo.classList.add('discoverPlace-img');
 
+        learn.textContent = "Learn More";
+        learn.addEventListener('click', () => {
+            window.open(place.learn, '_blank')
+        });
+
         address.textContent = place.address;
-        cost.textContent = place.cost;
         description.textContent = place.description;
 
+        figure.appendChild(photo);
+
         card.appendChild(name);
-        card.appendChild(photo);
-        card.appendChild(cost);
+        card.appendChild(figure);
         card.appendChild(description);
         card.appendChild(address);
+        card.appendChild(learn);
 
         discoverPlace.appendChild(card);
     });
 
 }
 
+function displayLastVisit() {
+    const lastVisit = localStorage.getItem('lastVisit');
+    const now = Date.now();
+
+    if (!lastVisit) {
+        visitMessage.textContent = 'Welcome! Let us know if you have any questions.';
+    } else {
+        const lastVisitTime = parseInt(lastVisit);
+        const visitMath = now - lastVisitTime;
+        const visitDays = Math.floor(visitMath / (1000 * 60 * 60 * 24));
+
+        if (visitDays < 1) {
+            visitMessage.textContent = 'Back so soon! Awesome!';
+        } else {
+            visitMessage.textContent = 'You last visited ' + visitDays + ' days ago.'
+        }
+    }
+
+    localStorage.setItem('lastVisit', now);
+}
+
 getPlaceData();
+displayLastVisit();
