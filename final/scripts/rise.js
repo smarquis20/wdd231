422382efbd53787830b3ce38d2eaac7d521b129e
smarquis20 <shaunmarquis@gmail.com>
2025-02-12 00:28:00 -0600

    final project update

--- final/scripts/rise.js
+++ final/scripts/rise.js
@@ -1,2 +1,26 @@
-document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById('currentyear').innerHTML = new Date().getFullYear();
+document.getElementById('lastModified').innerHTML = document.lastModified;
+
+document.addEventListener('DOMContentLoaded', function () {
+    const nav = document.querySelector('.nav-menu');
+    const header = document.querySelector('header');
+    const missionContainer = document.querySelector('.mission-container');
+
+    function updateNavPosition() {
+        const headerHeight = header.offsetHeight;
+        const navHeight = nav.offsetHeight;
+        isLargeScreen = window.innerWidth >= 720;
+
+        if (window.scrollY > headerHeight && isLargeScreen) {
+            nav.className = nav.className.replace('nav-menu', 'fixed-nav');
+            missionContainer.style.marginTop = `${navHeight}px`;
+        } else {
+            nav.className = nav.className.replace('fixed-nav', 'nav-menu');
+            missionContainer.style.marginTop = '0';
+        }
+
+    }
+
+    window.addEventListener('scroll', updateNavPosition);
+    window.addEventListener('resize', updateNavPosition);
+});
