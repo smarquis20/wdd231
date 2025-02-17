2c09e0512dc7c4205162d6645a1a55d4c9526cfa
smarquis20 <shaunmarquis@gmail.com>
2025-02-17 15:37:24 -0600

    final project updates

--- final/scripts/about.js
+++ final/scripts/about.js
@@ -1,5 +1,6 @@
 document.getElementById('currentyear').innerHTML = new Date().getFullYear();
 document.getElementById('lastModified').innerHTML = document.lastModified;
+const staffBio = document.querySelector('#staffBio');
 
 document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('.nav-menu');
@@ -30,3 +31,42 @@ document.addEventListener("DOMContentLoaded", function () {
     let now = new Date();
     timeStampField.value = now.toISOString();
 });
+
+const url = 'data/aboutus.json';
+
+async function getBioData() {
+   const response = await fetch(url);
+   const data = await response.json();
+   members = data.members;
+
+   displayMembers(members);
+}
+
+const displayMembers = (members) => {
+
+    members.forEach((member) => {
+        let card = document.createElement('div');
+        let photo = document.createElement('img');
+        let title = document.createElement('h2');
+        let name = document.createElement('p');
+        let email = document.createElement('p');
+
+        card.classList.add('staffBioCard');
+
+        name.innerHTML = `<strong>Name:</strong> ${member.name}`;
+        photo.setAttribute('src', member.image);
+        photo.setAttribute('alt', ` ${member.name}`);
+        photo.setAttribute('loading', 'lazy');
+        title.textContent = member.title;
+        email.innerHTML = `<strong>Email:</strong> ${member.email}`;
+
+        card.appendChild(photo);
+        card.appendChild(title);
+        card.appendChild(name);
+        card.appendChild(email);
+
+        staffBio.appendChild(card);
+    });
+}
+
+getBioData();
